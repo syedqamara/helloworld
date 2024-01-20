8d8e1bd21a5d97b7013bb482731db6d46ef840f2
Nasrullah <24110307@lums.edu.pk>
2024-01-20 19:47:23 +0500

    Create Table.py

---
+++ Table.py
@@ -0,0 +1,21 @@
+x = int(input("Enter Table no: "))
+
+print(str(x) + " x 1 = " + str(x * 1))
+
+print(str(x) + " x 2 = " + str(x * 2))
+
+print(str(x) + " x 3 = " + str(x * 3))
+
+print(str(x) + " x 4 = " + str(x * 4))
+
+print(str(x) + " x 5 = " + str(x * 5))
+
+print(str(x) + " x 6 = " + str(x * 6))
+
+print(str(x) + " x 7 = " + str(x * 7))
+
+print(str(x) + " x 8 = " + str(x * 8))
+
+print(str(x) + " x 9 = " + str(x * 9))
+
+print(str(x) + " x 10 = " + str(x * 10))
