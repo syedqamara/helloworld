a5ea89870a4394d0ff97e85260b93dc84b6b471e
Qamar Abbas <syedqamar.a1@gmail.com>
2024-01-20 19:57:02 +0500

    Update Table.py

--- Table.py
+++ Table.py
@@ -1,21 +1,17 @@
 x = int(input("Enter Table no: "))
 
-print(str(x) + " x 1 = " + str(x * 1))
+def printTables(table, index):
+    print( str(table) + " x " + str(index) + " = " + str(table * index) )
 
-print(str(x) + " x 2 = " + str(x * 2))
+printTables(x, 1)
+printTables(x, 2)
+printTables(x, 3)
+printTables(x, 4)
+printTables(x, 5)
+printTables(x, 6)
+printTables(x, 7)
+printTables(x, 8)
+printTables(x, 9)
+printTables(x, 10)
 
-print(str(x) + " x 3 = " + str(x * 3))
 
-print(str(x) + " x 4 = " + str(x * 4))
-
-print(str(x) + " x 5 = " + str(x * 5))
-
-print(str(x) + " x 6 = " + str(x * 6))
-
-print(str(x) + " x 7 = " + str(x * 7))
-
-print(str(x) + " x 8 = " + str(x * 8))
-
-print(str(x) + " x 9 = " + str(x * 9))
-
-print(str(x) + " x 10 = " + str(x * 10))
