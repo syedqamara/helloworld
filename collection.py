c0f1feacba8cc718ba5d1d326a4bebe2c4b30995
Qamar Abbas <syedqamar.a1@gmail.com>
2024-01-22 21:37:43 +0500

    Loop with dynamic range

--- collection.py
+++ collection.py
@@ -1,7 +1,9 @@
 
+x = int(input("Enter Limit to find Sum of Natural Number from Zero to This: "))
+
 sum = 0
 
-for num in range(0, 5):  # Range from 1 to 4 (5 is not included)
+for num in range(0, x):  # Range from 1 to 4 (5 is not included)
     print(str(num))
     sum = sum + num
 
